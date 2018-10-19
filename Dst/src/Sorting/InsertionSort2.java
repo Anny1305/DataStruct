a58525ce33bef8636b600b81b983689dddc3082c
Anny <talktoanny@gmail.com>
2018-10-19 12:02:01 +0530

    Move to Sorting package

--- Dst/src/Sorting/InsertionSort2.java
+++ Dst/src/Sorting/InsertionSort2.java
@@ -1,23 +1,22 @@
+package Sorting;
 import java.util.Arrays;
 
-public class InsertionSort {
+public class InsertionSort2 {
 	
 	static void Sort(int [] arr)
 	{
 		
-		for(int i=0;i<arr.length-1;i++)
+		for(int j=1;j<arr.length;j++)
 		{
-			for(int j=i+1;j>0;j--)
+			int key=arr[j];
+			int i=j-1;
+			
+			while(i>=0 && arr[i]>key)
 			{
-				if(arr[j]<arr[j-1])
-				{
-					int temp=arr[j];
-					arr[j]=arr[j-1];
-					arr[j-1]=temp;
-				}
-				else 
-					break;
+				arr[i+1]=arr[i];
+				i--;
 			}
+			arr[i+1]=key;
 		}
 		
 	}
