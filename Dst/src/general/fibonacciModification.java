3e78f9edcf4e57d202e18cb4d68f99acfbd9c1a4
Anny <anny.kumar@care.com>
2021-01-18 13:32:37 +0530

    code change

--- Dst/src/general/fibonacciModification.java
+++ Dst/src/general/fibonacciModification.java
@@ -1,5 +1,6 @@
 package general;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,27 +11,27 @@ public class fibonacciModification {
 		int result=0;
 		if(memo[n]!=0)
 			return memo[n];
-		
+
 		if(n ==0) result= 0;
 		if(n ==1 || n==2)
 			 result = 1;
 		else
 			 result = fib(n-1,memo)+fib(n-2,memo);
 		memo[n]=result;
-		
+
 		return result;
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		
-	  System.out.println("Find the Number of fibonacci sequence ");	
+
+	  System.out.println("Find the Number of fibonacci sequence ");
 	  Scanner in = new Scanner(System.in);
 	  int num = in.nextInt();
 	  int[] memo =new int[num+1];
-	  
+
 	  //System.out.println("Number is : "+fib(num,memo));
-	  List<String> l = List.of("a");
+	  List<String> l = Arrays.asList("a");
 	  System.out.println(l.getClass().getName());
 	  
 		
